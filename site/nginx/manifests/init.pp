3cac4e999ce9387fdde024ee81ed31e8cc117078
lnbertagnolli <lee@bertagnolli.net>
2016-05-26 08:10:51 -0500

    all of Andrew's new crap

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,128 +1,128 @@
 class nginx {
-  yumrepo { 'base': 
-     ensure              => 'present', 
-     descr               => 'CentOS-$releasever - Base', 
-     enabled             => '1', 
-     gpgcheck            => '1', 
-     gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7', 
-     mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra', 
-     priority            => '99', 
-     skip_if_unavailable => '1', 
-     before     => [ Package['nginx'], Package['openssl-libs'] ], 
-   } 
-    
-   yumrepo { 'updates': 
-     ensure              => 'present', 
-     descr               => 'CentOS-$releasever - Updates', 
-     enabled             => '1', 
-     gpgcheck            => '1', 
-     gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7', 
-     mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra', 
-     priority            => '99', 
-     skip_if_unavailable => '1', 
-     before     => [ Package['nginx'], Package['openssl-libs'] ], 
-   } 
-    
-   yumrepo { 'extras': 
-     ensure              => 'present', 
-     descr               => 'CentOS-$releasever - Extras', 
-     enabled             => '1', 
-     gpgcheck            => '1', 
-     gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7', 
-     mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra', 
-     priority            => '99', 
-     skip_if_unavailable => '1', 
-     before     => [ Package['nginx'], Package['openssl-libs'] ], 
-   } 
-    
-   yumrepo { 'centosplus': 
-     ensure     => 'present', 
-     descr      => 'CentOS-$releasever - Plus', 
-     enabled    => '1', 
-     gpgcheck   => '1', 
-     gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7', 
-     mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus&infra=$infra', 
-     before     => [ Package['nginx'], Package['openssl-libs'] ], 
-   } 
-
-   package { ['openssl', 'openssl-libs'] :
-     ensure => '1.0.1e-51.el7_2.5',
-     before => Package['nginx'],
-   }
-   
-   file { 'nginx rpm' :
-    ensure   => file,
-    path     => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
-    source   => 'puppet:///modules/nginx/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+  case $::osfamily {
+    'redhat','debian': {
+      $package = 'nginx'
+      $owner   = 'root'
+      $group   = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir  = '/var/log/nginx'
+    }
+    'windows': {
+      $package = 'nginx-service'
+      $owner   = 'Administrator'
+      $group   = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $logdir  = 'C:/ProgramData/nginx/logs'
+    }
+    default : {
+      fail("Module ${module_name} is not intended to run on ${::osfamily}")
+    }
   }
 
-  package { 'nginx' :
-    ensure   => '1.6.2-1.el7.centos.ngx',
-    source   => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
-    provider => rpm,
-    require  => File['nginx rpm'],
+  $runas_user = $::osfamily ? {
+    'redhat'  => 'nginx',
+    'debian'  => 'www-data',
+    'windows' => 'nobody',
   }
 
-  file { 'docroot':
-    path => '/var/www',
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    require => Package['nginx'],
+  if $::virtual {
+    $virt_name = capitalize($::virtual)
   }
-  
-  file { 'index':,
-    path => '/var/www/index.html',
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    require => File['docroot'],
-    source => 'puppet:///modules/nginx/index.html',
+
+  File {
+    owner   => $owner,
+    group   => $group,
+    mode    => '0664',
   }
-  
-  file { 'nginxconfigdir':
-    path => '/etc/nginx',
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    require => Package['nginx'],
+
+  if $::osfamily == 'RedHat' {
+    Yumrepo {
+      ensure              => present,
+      enabled             => '1',
+      gpgcheck            => '1',
+      gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+      priority            => '99',
+      skip_if_unavailable => '1',
+      before              => [ Package['nginx'], Package['openssl-libs'] ],
+    }
+
+
+    yumrepo { 'base':
+      descr      => 'CentOS-$releasever - Base',
+      mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra',
+    }
+
+    yumrepo { 'updates':
+      descr      => 'CentOS-$releasever - Updates',
+      mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
+    }
+
+    yumrepo { 'extras':
+      descr      => 'CentOS-$releasever - Extras',
+      mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra',
+    }
+
+    yumrepo { 'centosplus':
+      descr      => 'CentOS-$releasever - Plus',
+      mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus&infra=$infra',
+    }
+
+    package { [ 'openssl', 'openssl-libs' ] :
+      ensure => '1.0.1e-51.el7_2.5',
+      before => Package['nginx'],
+    }
+
+    file { 'nginx rpm' :
+      ensure => file,
+      path   => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+      source => "puppet:///modules/${module_name}/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm",
+      before => Package['nginx'],
+    }
+
+    package { 'nginx' :
+      ensure   => '1.6.2-1.el7.centos.ngx',
+      source   => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+      provider => rpm,
+      require  => File['nginx rpm'],
+      before   => [ File['nginx conf'], File['default conf'] ],
+    }
   }
-  
-  file { 'nginxconfig':,
-    path    => '/etc/nginx/nginx.conf',
+
+  if $::osfamily != 'RedHat' {
+    package { $package :
+      ensure => present,
+    }
+  }
+
+  file { $docroot :
+    ensure  => directory,
+  }
+
+  file { 'index html' :
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => File['nginxconfigdir'],
-    source  => 'puppet:///modules/nginx/nginx.conf',
+    path    => "${docroot}/index.html",
+    content => template('nginx/index.html.erb'),
   }
-  
-  file { 'nginxdefaultdir':
-    path => '/etc/nginx/conf.d',
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    require => Package['nginx'],
+
+  file { 'nginx conf' :
+    ensure  => file,
+    path    => "${confdir}/nginx.conf",
+    content => template('nginx/nginx.conf.erb'),
   }
-  
-  file { 'nginxdefaultconf':
-    path => '/etc/nginx/conf.d/default.conf',
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    require => File['nginxdefaultdir'],
-    source => 'puppet:///modules/nginx/default.conf',
+
+  file { 'default conf' :
+    ensure  => file,
+    path    => "${confdir}/conf.d/default.conf",
+    content => template('nginx/default.conf.erb'),
   }
-  
-  service { 'nginx':
-    ensure => running,
-    require => Package['nginx'],
-    subscribe => File['nginxdefaultconf', 'nginxconfig', 'index']
+
+  service { 'nginx' :
+    ensure    => running,
+    enable    => true,
+    require   => File['index html'], 
+    subscribe => [ File['nginx conf'], File['default conf'] ],
   }
-}  
+
+}
